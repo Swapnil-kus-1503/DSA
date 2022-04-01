060b95cdf754d3fa291132e195a097ac7cec5ee9
Swapnil-kus-1503 <ashukushwaha15031997@gmail.com>
2022-04-01 10:48:42 +0530

    demo

--- bubblesort.js
+++ bubblesort.js
@@ -8,14 +8,31 @@ function swap(arr,j,min)
 var arr=[3,5,0,9,-3,8,-1];
 var N=arr.length;
 for(var i=0;i<N;i++)
+{
+  for(var j=0;j<N-i-1;j++)
   {
-    for(var j=0;j<N-i-1;j++)
-      {
-        if(arr[j]>arr[j+1])
-        {
-           swap(arr,j,j+1);        }
-      }
+    if(arr[j]<arr[j+1])
+    {
+      swap(arr,j,j+1);
+    }
   }
-// console.log(arr);
-
+}
 console.log(arr.join(" "));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
